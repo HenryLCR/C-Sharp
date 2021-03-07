b2b1225ad97dc2bd8dc3394bf1077c6dff013c4c
Henry La Chira <henrylachira1995@gmail.com>
2021-03-07 15:16:46 -0600

    Añadiendo Tarea #2 Unidad 2

---
+++ Tarea2-ejercicios/propiedades/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace propiedades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno a= new Alumno();
+            a.Id = 1;
+            a.PrimerNombre = "Luis";
+            a.SegundoNombre = "Alexander";
+            
+            Alumno b = new Alumno();
+            b.Id = 2;
+            b.PrimerNombre = "Maria";
+            b.SegundoNombre = "Cristina";
+
+            Alumno c = new Alumno(3);
+            
+            c.PrimerNombre = "Carlos";
+            c.SegundoNombre = "Arnoldo";
+
+            Alumno d = new Alumno ("Luis", "Alexander");
+
+
+            Console.WriteLine(a.Id);
+            Console.WriteLine(b.Id);
+            Console.WriteLine(c.Id);
+            Console.WriteLine(d.PrimerNombre + " " + d.SegundoNombre);
+
+        }
+    }
+}
